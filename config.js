38008d3e3442333c5fff20fbac110832f5424730
Pilek01 <pilek01@gmail.com>
2026-02-21 05:11:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 ﻿// Single source of truth for current build version.
-window.GAME_VERSION = "v0.2.25";
+window.GAME_VERSION = "v0.2.27";
 
 // Enable/disable debug cheat menu (F9).
 // true  = cheats enabled
